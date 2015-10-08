e06e382032d2a108e7cd8efbe0a703261df299d5
David Gonzalez <djgonza1@uci.edu>
2015-10-07 22:58:36 -0700

    Added first animation

---
+++ Scripts/MoveEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveEventHandler : MonoBehaviour 
+{
+
+
+	//Private members
+	bool onPunch = true;
+	Animator anim;
+
+	void Start () 
+	{
+		anim = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log("Punch");
+			onPunch = true;
+			anim.SetBool("OnPunch", true);
+			//anim.Play("Punch");
+			//anim.wrapMode = WrapMode.Once;
+		}
+	}
+
+	public void OnPunch()
+	{
+		onPunch = false;
+		anim.SetBool("OnPunch", false);
+	}
+}
