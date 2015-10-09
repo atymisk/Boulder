6271c5db0b30996d40abfe828c06e593c473019f
djgonza1 <djgonza1@uci.edu>
2015-10-08 18:36:19 -0700

    Added flying kick animation. Punch is U, Kick is I

--- Scripts/MoveEventHandler.cs
+++ Scripts/MoveEventHandler.cs
@@ -7,6 +7,7 @@ public class MoveEventHandler : MonoBehaviour
 
 	//Private members
 	bool onPunch = true;
+	bool onFlyingKick = true;
 	Animator anim;
 
 	void Start () 
@@ -17,19 +18,60 @@ public class MoveEventHandler : MonoBehaviour
 	// Update is called once per frame
 	void Update () 
 	{	
-		if (Input.GetKeyDown(KeyCode.Space))
+		if (Input.GetKeyDown(KeyCode.U))
 		{
-			Debug.Log("Punch");
-			onPunch = true;
-			anim.SetBool("OnPunch", true);
-			//anim.Play("Punch");
-			//anim.wrapMode = WrapMode.Once;
+			OnPunchStart();
+		}
+		else if(Input.GetKeyDown (KeyCode.I))
+		{
+			OnFlyingKickStart();
 		}
 	}
-
-	public void OnPunch()
+	
+	//Events
+	public void OnPunchStart()
 	{
+		Debug.Log("Punch");
+		onPunch = true;
+		anim.SetBool("OnPunch", onPunch);
+	}
+	
+	public void OnPunchEnd()
+	{
+		Debug.Log("OnPunchEnd");
 		onPunch = false;
-		anim.SetBool("OnPunch", false);
+		anim.SetBool("OnPunch", onPunch);
+	}
+	
+	public void OnFlyingKickStart()
+	{
+		Debug.Log ("FlyingKick");
+		onFlyingKick = true;
+		anim.SetBool("OnFlyingKick", onFlyingKick);
+		Vector2 speed = new Vector2(-30, 0);
+		StartCoroutine(MoveOverTime(speed));
+	}
+	
+	public void OnFlyingKickEnd()
+	{
+		onFlyingKick = false;
+		Debug.Log("OnFlyingKickEnd " + onFlyingKick.ToString());
+		anim.SetBool("OnFlyingKick", onFlyingKick);
+	}
+	
+	//Coroutines
+	IEnumerator MoveOverTime(Vector2 speed)
+	{
+		while(onFlyingKick)
+		{
+			float xDisplacement = speed.x * Time.deltaTime;
+			float yDisplacement = speed.y * Time.deltaTime;
+			this.transform.position = new Vector3(xDisplacement + this.transform.position.x, 
+												  yDisplacement + this.transform.position.y, 
+												  this.transform.position.z);
+												  
+			Debug.Log("moving " + this.transform.position.ToString());
+			yield return new WaitForEndOfFrame();
+		}
 	}
 }
