2bdb78ce1e533df3e5ddda4f0a0f722dcc599679
Harry <harrynp12@gmail.com>
2016-02-02 17:19:43 -0800

    fixed compiler error

--- Scripts/Manager/ControllerInputManager.cs
+++ Scripts/Manager/ControllerInputManager.cs
@@ -147,7 +147,7 @@ public class ControllerInputManager : InputManager
 						playerTwo.Jump ();
 					}
 					/*(Input.GetButton ("RightBumper_P2") ||*/
-					if (Input.GetButtonDown ("X_P2") && (Input.GetAxis("RightTrigger_P2") > 0 || Input.GetAxis ("LeftTrigger_P2"))) {
+					if (Input.GetButtonDown ("X_P2") && (Input.GetAxis("RightTrigger_P2") > 0 || Input.GetAxis ("LeftTrigger_P2") > 0)) {
 						playerTwo.RocketLeftArm ();
 					}else if (Input.GetButtonDown ("X_P2")) {
 						playerTwo.LeftPunch ();
