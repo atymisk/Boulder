aea760617a96d6d03b5efad03711bcaa6aed982f
Harry <harrynp12@gmail.com>
2016-02-04 17:59:26 -0800

    fixed compiler errors

--- Scripts/Manager/ControllerInputManager.cs
+++ Scripts/Manager/ControllerInputManager.cs
@@ -53,7 +53,7 @@ public class ControllerInputManager : InputManager
 					}
 					
 					if ((Input.GetAxis ("LeftJoystickY_P1") < 0 || Input.GetAxis("DPadY_P1") > 0) /*&& (previousP1Y == 0 || previousP1DPadY == 0)*/) {
-						Debug.Log (Input.GetAxis ("LeftJoystickY_P1"));
+//						Debug.Log (Input.GetAxis ("LeftJoystickY_P1"));
 						playerOne.Jump ();
 					}
 					
@@ -144,11 +144,11 @@ public class ControllerInputManager : InputManager
 						playerTwo.StayStill ();
 					}
 					
-					if ((Input.GetAxis ("LeftJoystickY_P2") < 0 || Input.GetAxis ("DPadY_P2") > 0) && (previousP2Y == 0 || previousP2DPadY == 0)) {
+					if ((Input.GetAxis ("LeftJoystickY_P2") < 0 || Input.GetAxis ("DPadY_P2") > 0) /*&& (previousP2Y == 0 || previousP2DPadY == 0)*/) {
 						playerTwo.Jump ();
 					}
 					/*(Input.GetButton ("RightBumper_P2") ||*/
-					if (Input.GetButtonDown ("X_P2") && (Input.GetAxis("RightTrigger_P2") > 0 /*|| Input.GetAxis ("LeftTrigger_P2") > 0)*/) {
+					if (Input.GetButtonDown ("X_P2") && (Input.GetAxis("RightTrigger_P2") > 0 || Input.GetAxis ("LeftTrigger_P2") > 0)) {
 						playerTwo.RocketLeftArm ();
 					}else if (Input.GetButtonDown ("X_P2")) {
 						playerTwo.LeftPunch ();
