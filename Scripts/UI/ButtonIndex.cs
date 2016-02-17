9d6e556b4cff073dac961d8ab3fb8d8cc3cbb390
Anthony Cheng <anthoc2@uci.edu>
2016-02-16 18:19:33 -0800

    fixed the issue with the controller delay and UI, changed UI behavior in the settings menu

---
+++ Scripts/UI/ButtonIndex.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ButtonIndex : MonoBehaviour, ISelectHandler
+{
+    public int index = 0;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        UIInputManager.index = this.index;
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Scripts/UI/UIInputManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIInputManager : MonoBehaviour
+{
+    //public enum SceneName { MAIN, SETTING, STAGE };
+    public Button[] btnlist;
+    public static int index = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        //btnlist[index].onClick.Invoke();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(index != 3)
+        {
+            Button[] minilist = btnlist[index].GetComponentsInChildren<Button>();
+	        if(Input.GetKeyDown(KeyCode.A) || Input.GetAxis("LeftJoystickX_P2") < 0 || Input.GetAxis("LeftJoystickX_P1") < 0 || Input.GetAxis("DPadX_P1") < 0)
+            {
+                minilist[1].onClick.Invoke();
+            }
+            if(Input.GetKeyDown(KeyCode.D) || Input.GetAxis("LeftJoystickX_P2") > 0 || Input.GetAxis("LeftJoystickX_P1") > 0 || Input.GetAxis("DPadX_P1") > 0)
+            {
+                minilist[2].onClick.Invoke();
+            }
+
+        }
+	}
+}
