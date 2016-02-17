05959d7051b576c19bb358384a0bdb3f5769c5e8
djgonza1 <djgonza1@uci.edu>
2016-02-16 18:30:45 -0800

    Improved phasing through items and players

--- Scripts/Robot/BodyDetector.cs
+++ Scripts/Robot/BodyDetector.cs
@@ -21,13 +21,13 @@ public class BodyDetector : MonoBehaviour {
 
 			float magnitude = 0;
 			if(this.transform.position.x >= partCenter.position.x) {
-				magnitude = -100f;
+				magnitude = -25f;
 			}
 			else {
-				magnitude = 100f;
+				magnitude = 25f;
 			}
-
-			partBody.AddForce(new Vector2(magnitude, 0), ForceMode2D.Force);
+            
+            partBody.AddForce(new Vector2(magnitude, 0), ForceMode2D.Force);
 		}
 
 		if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
@@ -37,13 +37,50 @@ public class BodyDetector : MonoBehaviour {
 			
 			float magnitude = 0;
 			if(this.transform.position.x >= partCenter.position.x) {
-				magnitude = -150f;
+				magnitude = -100f;
 			}
 			else {
-				magnitude = 150f;
+				magnitude = 100f;
 			}
-			
 			playerBody.AddForce(new Vector2(magnitude, 0), ForceMode2D.Force);
 		}
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Pickup"))
+        {
+            Rigidbody2D partBody = other.GetComponent<Rigidbody2D>();
+            Transform partCenter = other.transform.GetChild(1);
+            
+            float speed = 0;
+            if (this.transform.position.x >= partCenter.position.x)
+            {
+                speed = -50f;
+            }
+            else {
+                speed = 50f;
+            }
+
+            speed = partBody.velocity.x + speed;
+            partBody.velocity = new Vector2(speed, partBody.velocity.y);
+        }
+
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            Rigidbody2D playerBody = other.GetComponent<Rigidbody2D>();
+            Transform partCenter = other.transform.GetChild(1);
+            
+            float speed = 0;
+            if (this.transform.position.x >= partCenter.position.x)
+            {
+                speed = -20f;
+            }
+            else {
+                speed = 20f;
+            }
+            speed = playerBody.velocity.x + speed;
+            playerBody.velocity = new Vector2(speed, playerBody.velocity.y);
+        }
+    }
 }
