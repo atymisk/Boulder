f74f1b2d98fa85e002b31f89fa92e8c22800433b
Harry <harrynp12@gmail.com>
2016-02-25 13:11:31 -0800

    added x axis dead zone 0.7

--- Scripts/Manager/ControllerInputManager.cs
+++ Scripts/Manager/ControllerInputManager.cs
@@ -57,8 +57,8 @@ public class ControllerInputManager : InputManager
 					} else {
 						playerOne.StayStill ();
 					}
-					
-					if ((Input.GetAxis ("LeftJoystickY_P1") < 0  && previousP1Y == 0)  || (Input.GetAxis("DPadY_P1") > 0 && previousP1DPadY == 0)) {
+//					if ((Input.GetAxis ("LeftJoystickY_P1") < 0  && previousP1Y == 0)  || (Input.GetAxis("DPadY_P1") > 0 && previousP1DPadY == 0)) {
+					if ((Input.GetAxis ("LeftJoystickY_P1") < 0  && previousP1Y == 0 && Input.GetAxis ("LeftJoystickX_P2") > 0.7  && Input.GetAxis ("LeftJoystickX_P2") < -0.7) || (Input.GetAxis("DPadY_P1") > 0 && previousP1DPadY == 0)) {
 						playerOne.Jump ();
 					}
 
@@ -164,7 +164,8 @@ public class ControllerInputManager : InputManager
 						playerTwo.StayStill ();
 					}
 					
-					if ((Input.GetAxis ("LeftJoystickY_P2") < 0  && previousP2Y == 0)  || (Input.GetAxis("DPadY_P2") > 0 && previousP2DPadY == 0)) {
+//					if ((Input.GetAxis ("LeftJoystickY_P2") < 0  && previousP2Y == 0)  || (Input.GetAxis("DPadY_P2") > 0 && previousP2DPadY == 0)) {
+					if ((Input.GetAxis ("LeftJoystickY_P2") < 0  && previousP2Y == 0 && Input.GetAxis ("LeftJoystickX_P2") > 0.7  && Input.GetAxis ("LeftJoystickX_P2") < -0.7)  || (Input.GetAxis("DPadY_P2") > 0 && previousP2DPadY == 0)) {
 						playerTwo.Jump ();
 					}
 
