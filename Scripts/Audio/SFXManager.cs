e912d3b8734f6e8d9f2426a4c87fd29d3c66e54a
Anthony Cheng <anthoc2@uci.edu>
2016-03-03 14:56:12 -0800

    Adding in all the audio files that our MFA student sent and added the bgm to all scenes except the stages

---
+++ Scripts/Audio/SFXManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SFXManager : MonoBehaviour
+{
+    private static AudioClip MissingLimb;
+
+    private static AudioClip pickup1;
+    private static AudioClip pickup2;
+    private static AudioClip pickup2_long;
+
+    private static AudioClip limbflying1;
+    private static AudioClip limbflying2;
+    private static AudioClip limbflying3;
+    private static AudioClip limbflying4;
+    private static AudioClip limbflying5;
+    private static AudioClip limbflying6;
+    private static AudioClip limbflying7;
+
+    private static AudioClip MetalHit1;
+    private static AudioClip MetalHit2;
+    private static AudioClip MetalHit3;
+    private static AudioClip MetalHitGlass;
+    private static AudioClip MetalHitGlassBig;
+
+    private static AudioClip MetalScrape1;
+    private static AudioClip MetalScrape2;
+    private static AudioClip MetalScrape3;
+
+    private static AudioClip MetalScrapeH1;
+    private static AudioClip MetalScrapeH2;
+    private static AudioClip MetalScrapePass;
+
+    private static AudioClip StartEndFanFare1;
+    private static AudioClip StartEndFanFare2;
+
+    private static AudioClip Thruster1;
+    private static AudioClip Thruster2;
+    private static AudioClip Thruster3;
+    private static AudioClip Thruster4;
+    private static AudioClip ThrusterALL;
+    private static AudioClip ThrusterALL_V;
+
+    //AudioSource.timeSamples
+
+    // Use this for initialization
+    void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
