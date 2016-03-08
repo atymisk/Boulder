54b77c4f7b994b2cbdc9b123f22bf2902bd44804
Harry <harrynp12@gmail.com>
2016-03-08 14:45:07 -0800

    fixed controller sensitivity for settings screen

--- Scripts/UI/UIInputManager.cs
+++ Scripts/UI/UIInputManager.cs
@@ -7,25 +7,40 @@ public class UIInputManager : MonoBehaviour
     //public enum SceneName { MAIN, SETTING, STAGE };
     public Button[] btnlist;
     public static int index = 0;
+	private System.Diagnostics.Stopwatch stopwatch;
+	private bool canChange = false;
 	// Use this for initialization
 	void Start ()
     {
-        //btnlist[index].onClick.Invoke();
+		stopwatch = new System.Diagnostics.Stopwatch ();
+		//btnlist[index].onClick.Invoke();
+		stopwatch.Start ();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if(index != 3)
+		if (stopwatch.ElapsedMilliseconds > 250 && canChange) {
+			canChange = false;
+			stopwatch.Reset();
+		}
+
+        if(index != 3 && !canChange)
         {
             Button[] minilist = btnlist[index].GetComponentsInChildren<Button>();
-	        if(Input.GetKeyDown(KeyCode.A) || Input.GetAxis("LeftJoystickX_P2") < 0 || Input.GetAxis("LeftJoystickX_P1") < 0 || Input.GetAxis("DPadX_P1") < 0)
+	        if(Input.GetKeyDown(KeyCode.A) || Input.GetAxis("LeftJoystickX_P2") < 0 || Input.GetAxis("LeftJoystickX_P1") < 0)
             {
                 minilist[1].onClick.Invoke();
+				canChange = true;
+				stopwatch.Reset();
+				stopwatch.Start();
             }
-            if(Input.GetKeyDown(KeyCode.D) || Input.GetAxis("LeftJoystickX_P2") > 0 || Input.GetAxis("LeftJoystickX_P1") > 0 || Input.GetAxis("DPadX_P1") > 0)
+            if(Input.GetKeyDown(KeyCode.D) || Input.GetAxis("LeftJoystickX_P2") > 0 || Input.GetAxis("LeftJoystickX_P1") > 0)
             {
                 minilist[2].onClick.Invoke();
+				canChange = true;
+				stopwatch.Reset();
+				stopwatch.Start();
             }
 
         }
